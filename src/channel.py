cebb25865e8648e9906b424f2015640fde497343
eliseev <twicheg@gmail.com>
2023-06-05 18:26:07 +0300

    homework_2

--- src/channel.py
+++ src/channel.py
@@ -9,7 +9,7 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.channel_id = channel_id
+        self.__channel_id = channel_id
         self.youtube = build('youtube', 'v3', developerKey=Channel.API_KEY)
         self.channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
         self.title = self.channel['items'][0]['snippet']['title']
@@ -21,17 +21,20 @@ class Channel:
         self.view_count = self.channel['items'][0]["statistics"]["viewCount"]
 
     def print_info(self) -> None:
-        print(json.dump(self.channel))
+        print(json.dumps(self.channel))
 
-    # @property
-    # def channel_id(self):
-    #     return self.__channel_id
-    #
-    # @classmethod
-    # def get_service(cls, standart='UC-OVMPlMA3-YCIeg4z5z23A'):
-    #     return cls(standart)
+    @property
+    def channel_id(self):
+        return self.__channel_id
 
-    # def to_json(self, path):
-    #     with open(path, 'w') as file:
-    #         for a in [i for i in dir(self) if not i.startswith('_')]:
-    #             file.write = self.a
+    @classmethod
+    def get_service(cls, standart='UC-OVMPlMA3-YCIeg4z5z23A'):
+        return cls(standart)
+
+    @staticmethod
+    def to_json(path):
+        with open(path, 'w') as file:
+            a = Channel('UC-OVMPlMA3-YCIeg4z5z23A')
+            for attr in [i for i in dir(a) if not i.startswith('_')]:
+                if not attr == 'API_KEY':
+                    file.write(str(getattr(a, attr)))
