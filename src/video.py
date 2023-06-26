f1d3d0949c3d186c56817c3cad02ac93f70d7c77
eliseev <twicheg@gmail.com>
2023-06-27 01:45:15 +0300

    add try/except block

--- src/video.py
+++ src/video.py
@@ -6,16 +6,23 @@ class Video:
     """Класс Video"""
     def __init__(self, video_id: str) -> None:
         self.__video_id = video_id
-        self.video_link = 'http://youtube.com/watch?v=' + self.__video_id
         self.video_response = Channel.get_service().videos().list(part='snippet,statistics,contentDetails,topicDetails',
                                                                   id=self.__video_id).execute()
-        self.video_title: str = self.video_response['items'][0]['snippet']['title']
-        self.view_count: int = self.video_response['items'][0]['statistics']['viewCount']
-        self.like_count: int = self.video_response['items'][0]['statistics']['likeCount']
-        self.comment_count: int = self.video_response['items'][0]['statistics']['commentCount']
+        try:
+            self.video_link = 'http://youtube.com/watch?v=' + self.__video_id
+            self.title: str = self.video_response['items'][0]['snippet']['title']
+            self.view_count: int = self.video_response['items'][0]['statistics']['viewCount']
+            self.like_count: int = self.video_response['items'][0]['statistics']['likeCount']
+            self.comment_count: int = self.video_response['items'][0]['statistics']['commentCount']
+        except IndexError:
+            self.video_link = None
+            self.title: str = None
+            self.view_count: int = None
+            self.like_count: int = None
+            self.comment_count: int = None
 
     def __str__(self):
-        return self.video_title
+        return self.title
 
 
 class PLVideo(Video):
